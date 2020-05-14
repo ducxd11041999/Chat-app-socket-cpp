80947d367aa9292c4ac816e3283bbc8d97fe074f
Đức Bùi <ducxd1141999@gmail.com>
2020-05-14 17:24:14 +0700

    add config

---
+++ server/config/config.h
@@ -0,0 +1,18 @@
+//Database Configuration
+#define URI "tcp://127.0.0.1"
+#define USER "root"
+
+#define PASS "root"
+
+
+#define DB "test"
+
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <string>
+using namespace std;
+
+#define SIZE_U 20
+#define SIZE_P 20
+#define SIZE_MESS 4096
